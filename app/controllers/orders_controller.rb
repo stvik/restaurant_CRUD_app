835ffcae2e1a42c6561f04cd2bfafa6af0efb176
github username <github email address>
2019-11-25 16:05:41 -0500

    building second join table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,40 @@
 class OrdersController < ApplicationController
+    
+    before_action :find_order, only: [:show, :edit, :update, :destroy]
+
+    def new
+        @order = Order.new
+    end
+
+    def show
+    end
+
+    def create
+        order = Order.create(order_strong_params)
+        redirect_to order_path(order)
+    end
+    
+    def edit
+    end
+
+    def update
+        order = Order.update(order_strong_params)
+        redirect_to order_path(order)
+    end
+
+    def destroy
+        @order.destroy
+        redirect_to orders_path
+    end
+
+    private
+
+    def find_order
+        @order = Order.find(params[:id])
+    end
+
+    def order_strong_params
+        params.require(:order).permit(:customer_id, :menu_item_id)
+    end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :menu_item
+
+  def date_format
+    I18n.l(self.created_at.in_time_zone("Eastern Time (US & Canada)"), format: :long)
+  end
 end
